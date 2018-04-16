db983ae33c773afb1647bb897204cc7bc9d1dd3e
Machiavellii <33911723+Machiavellii@users.noreply.github.com>
2018-04-16 19:14:29 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,59 @@
+// Initial Ratings
+const ratings = {
+    sony: 4.7,
+    samsung: 3.4,
+    vizio: 2.3,
+    panasonic: 3.6,
+    phillips: 4.1
+  }
+
+  // Total Stars
+  const starsTotal = 5;
+
+  // Run getRatings when DOM loads
+  document.addEventListener('DOMContentLoaded', getRatings); 
+  //Form elements
+  let productSelect = document.getElementById('product-select');
+  let ratingControl = document.getElementById('rating-control');
+
+  //Product 
+  let product;
+
+  //Product select change
+  productSelect.addEventListener('change', (e) =>{
+    product = e.target.value;
+    //Enablng Rating Control
+    ratingControl.disabled = false;
+    ratingControl.value = ratings[product];
+  });
+
+  // Rating control change
+  ratingControl.addEventListener('blur', (e) => {
+      let rating = e.target.value;
+
+      if(rating > 5){
+        alert("Please rate 1 - 5")
+        return;
+      }
+
+      //Change rating
+      ratings[product] = rating;
+      getRatings();
+  })
+  
+  // Get ratings
+  function getRatings() {
+    for (let rating in ratings) {
+      // Get percentage
+      const starPercentage = (ratings[rating] / starsTotal) * 100;
+
+      // Round to nearest 10
+      const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
+       
+      // Set width of stars-inner to percentage
+      document.querySelector(`.${rating} .stars-inner`).style.width = starPercentageRounded;
+
+      //Add number rating
+      document.querySelector(`.${rating} .number-rating`).innerHTML = ratings[rating];
+    }
+  }
